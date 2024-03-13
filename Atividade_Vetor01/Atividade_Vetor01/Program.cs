4ed7ce288e05e91b32dd74d5e51bd8dbac29642a
ArthurQuintino <arthurquintino2304@gmail.com>
2024-03-12 23:12:40 -0300

    primeiro commit

---
+++ Atividade_Vetor01/Atividade_Vetor01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Vetor01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numeros = new int[20];
+            int i;
+            for (i = 0; i < Numeros.Length; i++)
+            {
+                Numeros[i] = i + 1;
+                Console.WriteLine(Numeros[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
